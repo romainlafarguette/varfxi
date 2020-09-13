9f3406cfc292d22ea2b8415a967b8a8d1db8b7e5
Romain Lafarguette <romain.lafarguette@gmail.com>
2020-09-13 01:23:41 -0400

    New LateX charts

--- latex/auto/slides.el
+++ latex/auto/slides.el
@@ -3,6 +3,14 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "english") ("inputenc" "utf8") ("fontenc" "T1") ("caption" "font=scriptsize" "labelfont=scriptsize" "labelfont={color=imfblue}")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "../output/regressions_table_short"
@@ -30,12 +38,12 @@
     "import"
     "appendixnumberbeamer"
     "natbib")
-   (TeX-add-symbols
-    '("myframe" 1))
    (LaTeX-add-environments
     "largeitemize"
     "largeenumerate"
     "wideitemize"
-    "wideenumerate"))
+    "wideenumerate")
+   (LaTeX-add-xcolor-definecolors
+    "imfblue"))
  :latex)
 
